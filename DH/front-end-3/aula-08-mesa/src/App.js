1f80b498b87a887bd287e7aa18d9a8b0905ed5b0
gustavo barretto <gustavobarretto@gmail.com>
2021-11-08 10:57:10 -0300

    react-aula-08

---
+++ DH/front-end-3/aula-08-mesa/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './App.css';
+import Produtos from "./components/Produto";
+
+export default class App extends Component {
+
+  render () {
+    return <>
+    <Produtos />
+    </>;
+  }
+}
---
+++ DH/front-end-3/aula-08-mesa/src/components/Produto/index.jsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+export default class Produtos extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listagemDeProdutos: function listandoProdutos() {
+          const criandoProduto = (url, nome, key) => {
+              return {
+                  id: key,
+                  imgProduto: url,
+                  nomeProduto: nome
+              }
+          }
+          return [
+            criandoProduto("https://static.paodeacucar.com/img/uploads/1/828/528828.jpg" , "Feijao", 1),
+            criandoProduto("https://static.paodeacucar.com/img/uploads/1/828/528828.jpg" , "Arroz", 2),
+            criandoProduto("https://static.paodeacucar.com/img/uploads/1/828/528828.jpg" , "Alface", 3)
+          ]
+      },
+    };
+  }
+
+  removeProduto = (idProduto) => {
+    const listaNovaProdutos = this.state.listagemDeProdutos().filter(({ id }) => {
+      return id !== idProduto;
+    });
+
+    return this.setState({
+        listagemDeProdutos: listaNovaProdutos
+    }
+    );
+  };
+
+  checkingFunction() {
+    return typeof listagemDeProdutos === 'function' ? this.listagemDeProdutos() : this.listagemDeProdutos;
+  }
+
+  render() {
+    return (
+      <>
+        {/* Incremental */}
+        <div className='d-flex flex-wrap flex-column justify-content-center align-items-center py-3 my-4'>
+          {this.state.checkingFunction().map(({ id, imgProduto, nomeProduto }) => {
+            return (
+              <div key={id} onClick={() => this.removeProduto(id)}>
+                <h2>{nomeProduto}</h2>
+                <img src={imgProduto} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
