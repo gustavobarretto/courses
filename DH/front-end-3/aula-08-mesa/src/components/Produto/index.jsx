51689f05a5b89aeeec308ddec43defbb00d20139
gustavo barretto <gustavobarretto@gmail.com>
2021-11-08 11:05:40 -0300

    final adjusting

--- DH/front-end-3/aula-08-mesa/src/components/Produto/index.jsx
+++ DH/front-end-3/aula-08-mesa/src/components/Produto/index.jsx
@@ -4,7 +4,7 @@ export default class Produtos extends Component {
   constructor() {
     super();
     this.state = {
-      listagemDeProdutos: function listandoProdutos() {
+      listagemDeProdutos: (function listandoProdutos() {
           const criandoProduto = (url, nome, key) => {
               return {
                   id: key,
@@ -17,12 +17,12 @@ export default class Produtos extends Component {
             criandoProduto("https://static.paodeacucar.com/img/uploads/1/828/528828.jpg" , "Arroz", 2),
             criandoProduto("https://static.paodeacucar.com/img/uploads/1/828/528828.jpg" , "Alface", 3)
           ]
-      },
+      })(),
     };
   }
 
   removeProduto = (idProduto) => {
-    const listaNovaProdutos = this.state.listagemDeProdutos().filter(({ id }) => {
+    const listaNovaProdutos = this.state.listagemDeProdutos.filter(({ id }) => {
       return id !== idProduto;
     });
 
@@ -32,16 +32,12 @@ export default class Produtos extends Component {
     );
   };
 
-  checkingFunction() {
-    return typeof listagemDeProdutos === 'function' ? this.listagemDeProdutos() : this.listagemDeProdutos;
-  }
-
   render() {
     return (
       <>
         {/* Incremental */}
         <div className='d-flex flex-wrap flex-column justify-content-center align-items-center py-3 my-4'>
-          {this.state.checkingFunction().map(({ id, imgProduto, nomeProduto }) => {
+          {this.state.listagemDeProdutos.map(({ id, imgProduto, nomeProduto }) => {
             return (
               <div key={id} onClick={() => this.removeProduto(id)}>
                 <h2>{nomeProduto}</h2>
