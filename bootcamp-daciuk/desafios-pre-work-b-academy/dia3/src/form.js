24c535864ee908214423f617c38e81ad6b7f30f0
gustavo barretto <gustavobarretto@gmail.com>
2021-11-11 10:17:12 -0300

    bootcamp-daciuk

--- bootcamp-daciuk/desafios-pre-work-b-academy/dia3/src/form.js
+++ bootcamp-daciuk/desafios-pre-work-b-academy/dia3/src/form.js
@@ -1,8 +1,20 @@
 const inputName = document.querySelector("[data-js='name_input']");
 
 inputName.addEventListener('input', (e) => {
-  console.log(e.target.value)
+  const dontChange = ["da", "das", "de", "do", "dos"];
+  const valueAsArray = e.target.value.split(" ");
+  console.log(valueAsArray)
 
+  e.target.value = valueAsArray
+    .map((word) => {
+      return dontChange.includes(word.toLowerCase())
+        ? word.toLowerCase()
+        : capitalize(word);
+    })
+    .join(" ");
 
-  
+  function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }
+
 })
