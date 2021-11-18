5f332e71822839d8a8226abb375c04d244acde79
gustavo barretto <gustavobarretto@gmail.com>
2021-11-18 09:15:46 -0300

    dockerjenkins

---
+++ DH/infra-2/docker-spring-jenkins/dockerjenkins/src/main/java/com/example/dockerjenkins/DockerjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinsApplication.class, args);
+	}
+
+}
---
+++ DH/infra-2/docker-spring-jenkins/dockerjenkins/src/test/java/com/example/dockerjenkins/DockerjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
