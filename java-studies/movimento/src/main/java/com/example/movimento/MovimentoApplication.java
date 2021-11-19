9f5b94942ecafe2edef98af0f1404904ce9568f7
gustavo barretto <gustavobarretto@gmail.com>
2021-11-19 14:31:00 -0300

    java's studies

---
+++ java-studies/movimento/src/main/java/com/example/movimento/MovimentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.movimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimentoApplication.class, args);
+	}
+
+}
---
+++ java-studies/movimento/src/test/java/com/example/movimento/MovimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
