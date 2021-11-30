ad658849b04ab77b9ff889904964853293a25777
gustavo barretto <gustavobarretto@gmail.com>
2021-11-30 18:27:47 -0300

    java's studies

--- java-studies/aula-34-integradora/src/main/java/com/example/aula34integradora/controller/PacoteController.java
+++ java-studies/aula-34-integradora/src/main/java/com/example/aula34integradora/controller/PacoteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pacote")
+@RequestMapping("/pacotes")
 public class PacoteController {
 
     @Autowired
