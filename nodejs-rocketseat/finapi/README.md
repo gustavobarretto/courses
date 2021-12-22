6dbb267fcb7bac125bb2b0730584181d09324bcf
gustavo barretto <gustavobarretto@gmail.com>
2021-12-22 07:29:35 -0300

    finapi-rocketseat

--- nodejs-rocketseat/finapi/README.md
+++ nodejs-rocketseat/finapi/README.md
@@ -4,20 +4,21 @@
 
 ### Requisitos
 - [x] Deve ser possível criar umma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
-- [] Deve ser possível realizar um depósito
-- [] Deve ser possível realizar um saque
-- [] Deve ser possível buscar o extrato bancário do cliente por data
-- [] Deve ser possível atualizar dados da conta do cliente
-- [] Deve ser possível obter dados da conta do cliente
-- [] Deve ser possível deletar uma conta
+- [x] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível realizar um depósito
+- [x] Deve ser possível realizar um saque
+- [x] Deve ser possível buscar o extrato bancário do cliente por data
+- [x] Deve ser possível atualizar dados da conta do cliente
+- [x] Deve ser possível obter dados da conta do cliente
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível retornar o método balance
 
 ---
 
 ## Regras de negócio
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
-- [] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
-- [] Não deve ser possível fazer saque em uma conta não existente
-- [] Não deve ser possível excluir uma conta não existente
-- [] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível fazer depósito em uma conta não existente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
+- [x] Não deve ser possível fazer saque em uma conta não existente
+- [x] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível excluir uma conta não existente
--- nodejs-rocketseat/finapi/src/index.js
+++ nodejs-rocketseat/finapi/src/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 
@@ -12,11 +13,40 @@ const customers = [];
 // id - uuid;
 // statment []
 
+// Middleware
+
+function verifyExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer =  customers.find( customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found!" }); 
+    }
+
+    request.customer = customer;
+
+    return next();
+} 
+
+function getBalance(statement) {
+    const balance = statement.reduce( (acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount
+        }
+        else {
+            return acc - operation.amount
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
     const customerAlreadyExists = customers.some( 
-        (constumers) => constumers.cpf === cpf
+        (customer) => customer.cpf === cpf
     );
 
     if(customerAlreadyExists) {
@@ -27,24 +57,101 @@ app.post("/account", (request, response) => {
         cpf,
         name,
         id: uuidv4(),
-        statment: []
+        statement: []
     });
 
     return response.status(201).send()
     
 });
 
-app.get("/statement/", (request, response) => {
-    const { cpf } = request.headers;
+app.get("/statement/", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
-    const customer =  customers.find( customer => customer.cpf === cpf);
+    return response.json(customer.statement);
+})
 
-    if(!customer) {
-        return response.status(400).json({ error: "Customer not found!" }); 
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
     }
 
+    customer.statement.push(statementOperation);
 
-    return response.json(customer.statment);
+    return response.status(201).send();
+})
+
+app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
+})
+
+app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter( statement => {
+        return statement.created_at.toDateString() === new Date(dateFormat).toDateString();
+    })
+
+    return response.json(customer.statement);
+})
+
+app.put("/account", verifyExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send()
+})
+
+app.get("/account", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer)
+})
+
+app.delete("/account", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    //splice
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+})
+
+app.get("/balance", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
 })
 
 app.listen(3333);
