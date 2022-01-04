6f51a3641fa80a15aa754119494312cfe8903cfc
gustavo barretto <gustavobarretto@gmail.com>
2022-01-04 14:59:57 -0300

    java studies

---
+++ java-studies/challenge-meli/src/main/main.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+
+    public static void main(String[] args) {
+        Integer maxDigit = 7;
+        Integer maxSum = 21;
+
+        List<Integer> numbers = new ArrayList<>();
+
+        Integer maxNumber = maxDigit * 1111;
+
+        for (Integer index = 1000; index <= maxNumber; index++) {
+            String indexStr = index.toString();
+            List<Integer> arr = new ArrayList<>();
+
+            for (Integer word = 0; word <= 3; word++) {
+                if(Integer.parseInt(String.valueOf(indexStr.charAt(word))) <= maxDigit) {
+                    arr.add(Integer.parseInt(String.valueOf(indexStr.charAt(word))));
+                }
+            }
+            int result = arr.stream().reduce(0, (subtotal, element) -> subtotal + element);
+            if( result == 21) {
+                numbers.add(index);
+            }
+        }
+        System.out.println(numbers);
+    }
+}
