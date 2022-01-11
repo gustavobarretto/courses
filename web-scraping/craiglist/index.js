643c8e8d1287b90aa269f6ce746650d829320a4d
gustavo barretto <gustavobarretto@gmail.com>
2022-01-11 14:27:43 -0300

    webscrapping with axios

--- web-scraping/craiglist/index.js
+++ web-scraping/craiglist/index.js
@@ -1,13 +1,57 @@
-const request = require('request-promise');
+const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function scrapeCraiglist() {
+const url = 'https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof';
+
+const scrapeResults = []
+
+async function scrapeJobHeader() {
   try {
-    const htmlResult = await request.get('https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof');
-    console.log(htmlResult);
+    
+    const response = await axios.get(url);
+    const htmlResult = response.data;
+    
+    const $ = cheerio.load(htmlResult);
+
+    $('.result-info').each( (index, element) => {
+      const title = $(element).children('.result-heading').children('.result-title').text();
+      const url = $(element).children('.result-heading').children('.result-title').attr('href');
+      const datePosted = $(element).children('.result-date').attr('datetime');
+      const neighborhood = $(element).children('.result-meta').children('.result-hood').text().match(/\w+/gm).join(' ');
+      const scrapeResult = { title, url, datePosted, neighborhood };
+      scrapeResults.push(scrapeResult);
+
+    });
+
+    return scrapeResults;
+    
   } catch (err) {
     console.error(err);
   }
+}
+
+async function scrapeDescription(jobsWithHeaders) {
+  return await Promise.all(jobsWithHeaders.map( job => {
+    const response = await axios.get(job.url)
+    const htmlResult = response.data;
+    const $ = cheerio.load(htmlResult);
+
+    // Removing previously element
+    $('.print-qrcode-container').remove();
+
+    job.description = $('#posting').text();
+    job.compensation = $('body > section > section > section > div.mapAndAttrs > p > span:nth-child(1) > b').text();
+
+    return job;
+
+  })
+}
+
+async function scrapeCraigslist() {
+  const jobsWithHeaders = await scrapeJobHeader();
+  const jobsFullData = await scrapeDescription(jobsWithHeaders);
+
+  console.log(jobsFullData);
 }
 
 scrapeCraiglist();
