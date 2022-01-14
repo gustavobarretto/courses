93812b3836aa3c17ce5f17eab32818530e84dfbf
gustavo barretto <gustavobarretto@gmail.com>
2022-01-14 08:48:34 -0300

    webscrapping tdd

--- web-scraping/tdd-scraping/__test__/parser.test.js
+++ web-scraping/tdd-scraping/__test__/parser.test.js
@@ -1,7 +1,39 @@
 const { isTag } = require("domhandler")
 const parser = require("../parser")
+const fs = require("fs");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+let html;
+let listings;
 
-it("should give 4", () => {
-  const result = parser.add(2, 2);
-  expect(result).toBe(4);
+beforeAll( () => {
+  html = fs.readFileSync("./test.html");
+  listings = parser.listings(html)
+})
+
+// const listings = [{
+//   title: "Guitarist & 6 string bassist needed for new power trio",
+//   url: "https://sfbay.craigslist.org/sfc/muc/d/san-francisco-guitarist-string-bassist/7432652582.html",
+//   datePosted: new Date("2022-01-14"),
+//   hood: "SOMA / south beach"
+
+// }]
+
+it("should give the correct number of listings", () => {
+  expect(listings.length).toBe(120);
+})
+
+it("should get correct title", () => {
+  expect(listings[0].title).toBe("Guitarist & 6 string bassist needed for new power trio");
+})
+
+it("should get correct url", () => {
+  expect(listings[0].url).toBe("https://sfbay.craigslist.org/sfc/muc/d/san-francisco-guitarist-string-bassist/7432652582.html");
+})
+
+it("should get hood from listings correct", () => {
+  expect(listings[0].hood).toBe("(SOMA / south beach)")
+})
+
+it("should get correct date from listings", () => {
+  expect(listings[0].datePosted).toStrictEqual(new Date("2022-01-14 00:49"));
 })
--- web-scraping/tdd-scraping/parser.js
+++ web-scraping/tdd-scraping/parser.js
@@ -1,1 +1,20 @@
-exports.add = (number1, number2) => number1 + number2;
+const cheerio = require("cheerio");
+
+function getDatedPosted($, element) {
+  return new Date($(element).find(".result-date").attr("datetime"))
+}
+
+function getHood($, element) {
+  return $(element).find(".result-hood").text().trim();
+}
+
+exports.listings = (html) => {
+  const $ = cheerio.load(html);
+  return $(".result-info").map( (index, element) => {
+    const title = $(element).find(".result-title.hdrlnk").text();
+    const url = $(element).find(".result-title.hdrlnk").attr("href");
+    const hood = getHood($, element);
+    const datePosted = getDatedPosted($, element);
+    return { title, url, hood, datePosted }
+  }).get()
+}
