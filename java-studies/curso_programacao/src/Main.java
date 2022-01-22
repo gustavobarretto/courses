e9f575b1f51c7b536325fea6d50501729cd72dac
gustavo barretto <gustavobarretto@gmail.com>
2022-01-22 15:34:33 -0300

    java studies

--- java-studies/curso_programacao/src/Main.java
+++ java-studies/curso_programacao/src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Olá, mundo!");
-        
+        int y = 32;
+        double x = 10.35784;
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        Locale.setDefault(Locale.US);
+
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
     }
 }
