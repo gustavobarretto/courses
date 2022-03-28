4fa45e96bd87af94ea0c8af8fe5a093ef76ea934
Gustavo Barretto <gustavobarretto@gmail.com>
2022-03-28 18:26:00 -0300

    tdd mock studies

---
+++ java-studies/tdd-mock/src/main/java/com/example/tddmock/TddMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddMockApplication.class, args);
+	}
+
+}
---
+++ java-studies/tdd-mock/src/test/java/com/example/tddmock/TddMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
