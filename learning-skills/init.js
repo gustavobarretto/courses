a88030a7e157c55b1e95c2a0e4fd681af42c5e76
Gustavo Barretto <gustavobarretto@gmail.com>
2022-04-17 17:15:46 -0300

    adjustments

--- learning-skills/init.js
+++ learning-skills/init.js
@@ -1,9 +1,7 @@
 // const -> VARIÁVEL DE MEMÓRIA CONSTANTE.
 // let -> VARIÁVEL DE MEMÓRIA
-
 // DADOS PRIMÁRIOS -> string, integer, float, boolean
 // DADOS DE REFERÊNCIA -> objetos, arrays
-
 // métodos e funções e encapsulamento
 const obj = {
   99: "Sudeste",
