085326f71114f51aba7c203c46a0b09e47b2b5f4
Gustavo Barretto <gustavobarretto@gmail.com>
2022-04-19 23:25:57 -0300

    java's

---
+++ java-studies/nelio-91/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Student[] rooms = new Student[10];
+
+
+
+        sc.close();
+    }
+}
